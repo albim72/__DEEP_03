a1570265bafacb3b390bef9e59e38182b88f0051
albim72 <marcin.albiniak72@gmail.com>
2023-03-13 10:55:03 +0100

    Create mian.py

---
+++ DZIEN_1/py_podstawy/mian.py
@@ -0,0 +1,9 @@
+#import dane
+import dane as dn
+from dane import nr_filii as filia
+from dane import book as ksiazka
+
+
+print("________ WYŚWIETELENIE BEZPOŚREDNIE _____________")
+print(filia)
+print(ksiazka)
