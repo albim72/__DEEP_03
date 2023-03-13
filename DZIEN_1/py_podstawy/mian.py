6ca1903d41aec3a89b62c6f138449016cb472d52
albim72 <marcin.albiniak72@gmail.com>
2023-03-13 11:14:19 +0100

    Update mian.py

--- DZIEN_1/py_podstawy/mian.py
+++ DZIEN_1/py_podstawy/mian.py
@@ -2,8 +2,21 @@
 import dane as dn
 from dane import nr_filii as filia
 from dane import book as ksiazka
+from funkcje.bfunkcje import czytaj_liste, czytaj_slownik
+from klasy.cdane import CDane
 
 
 print("________ WYŚWIETELENIE BEZPOŚREDNIE _____________")
 print(filia)
 print(ksiazka)
+
+print("________ WYŚWIETELENIE ZA POMOCĄ FUNCKJI _____________")
+
+czytaj_liste(filia)
+czytaj_slownik(ksiazka)
+
+print("________ WYŚWIETELENIE ZA POMOCĄ OBIEKTU /Klasy/ _____________")
+cd = CDane(filia,ksiazka)
+
+cd.czytajlst()
+cd.czytajdict()
