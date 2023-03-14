240f99caaef188e86a71ce5f4b263a700f9f1d26
albim72 <marcin.albiniak72@gmail.com>
2023-03-14 10:23:57 +0100

    Create mian.py

---
+++ DZIEN_2/prosta_siec_neuro/mian.py
@@ -0,0 +1,21 @@
+import numpy as np
+from simplenauralnet import SimpleNeuralNetwork
+
+network = SimpleNeuralNetwork()
+
+print(network.weights)
+
+train_inputs = np.array([[1,1,0],[1,1,1],[1,1,0],[1,0,0],[0,1,1],[0,1,0],])
+
+train_outputs = np.array([[0,1,0,0,1,0]]).T
+train_iterations = 50000
+
+network.train(train_inputs,train_outputs,train_iterations)
+print(network.weights)
+
+print("testowanie danych")
+
+test_data = np.array([[1,1,1],[1,0,0],[0,1,1],[0,1,0],])
+
+for data in test_data:
+    print(f"wynik dla {data} wynosi: {network.propagation(data)}")
