a58ec3763b8be0b88c6eb8817b1108f1f080e489
albim72 <marcin.albiniak72@gmail.com>
2023-03-15 14:03:28 +0100

    Add files via upload

---
+++ models.py
@@ -0,0 +1,46 @@
+plotter = tfdocs.plots.HistoryPlotter(metric = 'binary_crossentropy', smoothing_std=10)
+plotter.plot(size_histories)
+plt.ylim([0.5, 0.7])
+
+
+#mały model
+
+small_model = tf.keras.Sequential([
+    # `input_shape` is only required here so that `.summary` works.
+    layers.Dense(16, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(16, activation='elu'),
+    layers.Dense(1)
+])
+
+size_histories['Small'] = compile_and_fit(small_model, 'sizes/Small')
+
+
+#sredni model
+
+medium_model = tf.keras.Sequential([
+    layers.Dense(64, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(64, activation='elu'),
+    layers.Dense(64, activation='elu'),
+    layers.Dense(1)
+])
+size_histories['Medium']  = compile_and_fit(medium_model, "sizes/Medium")
+
+
+#duzy model
+
+large_model = tf.keras.Sequential([
+    layers.Dense(512, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(512, activation='elu'),
+    layers.Dense(512, activation='elu'),
+    layers.Dense(512, activation='elu'),
+    layers.Dense(1)
+])
+
+size_histories['large'] = compile_and_fit(large_model, "sizes/large")
+
+
+plotter.plot(size_histories)
+a = plt.xscale('log')
+plt.xlim([5, max(plt.xlim())])
+plt.ylim([0.5, 0.7])
+plt.xlabel("Epochs [Log Scale]")
