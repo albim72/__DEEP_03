d674e001a68cf3a7489699bde6c7a9ae81a7c794
albim72 <marcin.albiniak72@gmail.com>
2023-03-15 13:48:43 +0100

    Add files via upload

---
+++ over.py
@@ -0,0 +1,40 @@
+lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
+  0.001,
+  decay_steps=STEPS_PER_EPOCH*1000,
+  decay_rate=1,
+  staircase=False)
+
+def get_optimizer():
+  return tf.keras.optimizers.Adam(lr_schedule)
+
+
+
+step = np.linspace(0,100000)
+lr = lr_schedule(step)
+plt.figure(figsize = (8,6))
+plt.plot(step/STEPS_PER_EPOCH, lr)
+plt.ylim([0,max(plt.ylim())])
+plt.xlabel('Epoch')
+_ = plt.ylabel('Learning Rate')
+
+
+def compile_and_fit(model, name, optimizer=None, max_epochs=10000):
+  if optimizer is None:
+    optimizer = get_optimizer()
+  model.compile(optimizer=optimizer,
+                loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                metrics=[
+                  tf.keras.losses.BinaryCrossentropy(
+                      from_logits=True, name='binary_crossentropy'),
+                  'accuracy'])
+
+  model.summary()
+
+  history = model.fit(
+    train_ds,
+    steps_per_epoch = STEPS_PER_EPOCH,
+    epochs=max_epochs,
+    validation_data=validate_ds,
+    callbacks=get_callbacks(name),
+    verbose=0)
+  return history
